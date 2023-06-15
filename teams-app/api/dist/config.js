937d8e206ab77bc3354ff153e36763f6f9dcc8b5
irwinwilliams <irwin.williams@gmail.com>
2023-06-15 00:03:26 -0400

    something's working

---
+++ teams-app/api/dist/config.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    teamsAppId: process.env.TEAMS_APP_ID,
+    authorityHost: process.env.M365_AUTHORITY_HOST,
+    tenantId: process.env.M365_TENANT_ID,
+    clientId: process.env.M365_CLIENT_ID,
+    clientSecret: "CKS8Q~LIVJtarf5gU8elJiNs-kaOnysnmAmfxbrx"
+};
+exports.default = config;
+/**
+ *
+ * const config = {
+  teamsAppId: process.env.TEAMS_APP_ID,
+  authorityHost: process.env.M365_AUTHORITY_HOST,
+  tenantId: process.env.M365_TENANT_ID,
+  clientId: process.env.M365_CLIENT_ID,
+  clientSecret: process.env.M365_CLIENT_SECRET,
+};
+
+export default config;
+
+ */ 
+//# sourceMappingURL=config.js.map
